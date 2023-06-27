521e04bf8a4d29bcc11c760367f62244034402a5
katyberg <hsinyi.berg@gmail.com>
2023-06-27 12:16:09 -0700

    update 144

--- leetcode/easy_144_binary_tree_preorder_traversal.py
+++ leetcode/easy_144_binary_tree_preorder_traversal.py
@@ -11,17 +11,17 @@ class Solution:
             return []
         return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
 
-        # Iteratively
-        answer = []
-        stack = [root]
-        # Note that we add curr_node's right child to the stack first.
-        while stack:
-            curr_node = stack.pop()
-            if curr_node:
-                answer.append(curr_node.val)
-                stack.append(curr_node.right)  # note that we can just push Null pointer on stack.
-                stack.append(curr_node.left)  # note that we can just push Null pointer on stack.
-        return answer
+        # # Iteratively
+        # answer = []
+        # stack = [root]
+        # # Note that we add curr_node's right child to the stack first.
+        # while stack:
+        #     curr_node = stack.pop()
+        #     if curr_node:
+        #         answer.append(curr_node.val)
+        #         stack.append(curr_node.right)  # note that we can just push Null pointer on stack.
+        #         stack.append(curr_node.left)  # note that we can just push Null pointer on stack.
+        # return answer
 
         # # Iteratively - REMEMBER WE ARE USING STACK TO MEMORIZE THE POINTER ORDERS!!!!!!!!!!!!!!!!
         # # BELOW IMPLEMENTATION IS WRONG BECAUSE WE NEED TO PUSH RRRRIIIIGGGGHHHHTTTT CHILD FIRST!!!!
