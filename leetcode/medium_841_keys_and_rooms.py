c7d33e7c2c8273890491e16714e149b45b293e23
katyberg <hsinyi.berg@gmail.com>
2023-07-09 00:04:21 -0700

    add 841

---
+++ leetcode/medium_841_keys_and_rooms.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        # Escapology :-)
+        n = len(rooms)
+        all_rooms = {r for r in range(n)}
+        visited_rooms = set()
+        def dfs(room):
+            if room not in visited_rooms:
+                visited_rooms.add(room)
+                keys = rooms[room]  # someone calls this "next_rooms" which is prorbably better
+                for key in keys:
+                    if key not in visited_rooms:
+                        dfs(key)
+        dfs(0)
+        return all_rooms == visited_rooms
+
